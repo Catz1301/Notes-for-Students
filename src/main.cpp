72d16b24e7e1d1d9254c49b485a8637941fccd95
Whiskers <33127788+Catz1301@users.noreply.github.com>
2019-12-06 08:48:21 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,10 @@
+// main.cpp
 
+#import <SFML/Graphics.hpp>
+#import "main.h"
+
+using namespace sf;
+
+doSplash() {
+	RenderWindow splashWindow(VideoMode(800, 600), "Notes for Students");
+	splashWindow.setStyle(sf::Style::None);
